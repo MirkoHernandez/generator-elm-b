62d57a2d37ce6b46423db63b4c3624c6a72cb4ed
Vagrant Default User <vagrant@stretch.localdomain>
2019-04-06 22:51:48 +0000

    Organize index.json

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,40 +4,38 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the world-class ${chalk.red('generator-elm-b')} generator!`)
-    );
+    prompting() {
+	// Have Yeoman greet the user.
+	this.log(
+	    yosay(`Welcome to the world-class ${chalk.red('generator-elm-b')} generator!`)
+	);
 
-    const prompts = [
-      {
-        type: 'input',
-        name: 'projectName',
-        message: 'Your project name',
-        default: this.appname
-      }
-    ];
+	const prompts = [
+	    {
+		type: 'input',
+		name: 'projectName', // Used in package.json
+		message: 'Your project name',
+		default: this.appname
+	    }
+	];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+	return this.prompt(prompts).then(props => {
+	    // To access props later use this.props.someAnswer;
+	    this.props = props;
+	});
+    }
 
     writing() {
-      this.fs.copy(
-	  this.templatePath('src'),
-	  this.destinationPath('./src')
-      );
-      this.fs.copy(
-	  this.templatePath('styles'),
-	  this.destinationPath('./styles')
-      );
+	// Project files and folders
 	this.fs.copy(
-	    this.templatePath('gitignore'),
-	    this.destinationPath('.gitignore')
+	    this.templatePath('src'),
+	    this.destinationPath('./src')
 	);
+	this.fs.copy(
+	    this.templatePath('styles'),
+	    this.destinationPath('./styles')
+	);
+	// Gulp build system
 	this.fs.copy(
 	    this.templatePath('gulpfile.js'),
 	    this.destinationPath('gulpfile.js')
@@ -46,18 +44,25 @@ module.exports = class extends Generator {
 	    this.templatePath('rollup.config.js'),
 	    this.destinationPath('rollup.config.js')
 	);
-      this.fs.copyTpl(
-      this.templatePath('package.json'),
-	this.destinationPath('package.json'),
-	this.props
-    );
-  }
-  install() {
-      this.installDependencies({
-	  yarn: true,
-	  npm: false,
-	  bower: false,
-      });
-  }
 
+	// Libraries
+	this.fs.copyTpl(
+	    this.templatePath('package.json'),
+	    this.destinationPath('package.json'),
+	    this.props
+	);
+	this.fs.copy(
+	    this.templatePath('gitignore'), 
+	    this.destinationPath('.gitignore')
+	);
+    }
+    
+    install() {
+	this.installDependencies({
+	    yarn: true,
+	    npm: false,
+	    bower: false,
+	});
+    }
+    
 };
